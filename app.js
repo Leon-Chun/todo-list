3f795c4b09add119796410e389ba4aa122ee7484
Leon-Chun <s1235753@gmail.com>
2022-09-29 21:00:40 +0800

    feat:add creat todo function

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
+const port = 3000
 const app = express()
 
-const port = 3000
+
+
 
 //database setting
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,6 +23,7 @@ db.once('open', () => {                // 連線成功
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
 
+app.use(express.urlencoded({extended:true}))
 
 //router setting
 app.get('/', (req, res) => {
@@ -28,6 +31,25 @@ app.get('/', (req, res) => {
     .lean()
     .then(todos => res.render('index',{todos}))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req,res) =>{
+  return res.render('new')
+})
+
+app.post('/todos', (req,res)=>{
+  //先拿出user的輸入值
+  const name = req.body.name
+  //再來新建 todo 使用寫好schema原則的Todo,
+  const todo = new Todo({
+    name
+    //會等同 name : name 寫法
+  })
+
+  //上面這段只存在聯覽器，接下來要送入 資料庫，使用save()
+  return todo.save()
+    .then(() => res.redirect('/'))
+    .catch( error => console.log(error) )
 })
 
 app.listen(port, () => {
